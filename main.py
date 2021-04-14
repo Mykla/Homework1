591d3bee4e26ab2352b487a3c2ed795fb8be029d
Mykla <mykla74@gmail.com>
2021-04-14 19:23:07 +0300

    Added store data into csv by folder

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 import json
 import yaml
-
+import errno
+import pandas as pd
+import os
 
 from requests import HTTPError
 
@@ -11,6 +13,14 @@ def load_config(config_path):
         conf = yaml.safe_load(yaml_file)
 
     return conf
+
+
+def create_folder(directory):
+    try:
+        os.makedirs(directory)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
 
 
 if __name__ == '__main__':
@@ -33,3 +43,14 @@ if __name__ == '__main__':
     # Request data
     r = requests.get(url, data=json.dumps(data), headers=headers)
     print(type(r.json()))
+
+    for_store = r.json()
+
+    # Store r json into folder
+    for i in range(len(for_store)):
+        my_dict = for_store[i]
+        df = pd.DataFrame(list(my_dict.items()), columns=['product_id', 'date'])
+        path = './data/' + for_store[i]['date']
+        create_folder(path)
+        csv_file_out = str(for_store[i]['product_id']) + '.csv'
+        df.to_csv(os.path.join(path, csv_file_out))
